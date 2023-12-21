385d5ef2b81cb7113c2a2b387779b647a6ba2a06
Rameshgandham <153059628+Rameshgandham@users.noreply.github.com>
2023-12-21 10:05:38 +0530

    Add files via upload
    commited first

---
+++ src/main/java/com/testing/Class1.java
@@ -0,0 +1,11 @@
+package com.testing;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my testing job is completed");
+
+	}
+
+}
