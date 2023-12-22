eb1e6cf83fd1a2b9212a7e8f77618b3db372c20c
Rameshgandham <rameshgamdham35@gmail.com>
2023-12-22 11:02:15 +0530

    i am in a git

---
+++ src/main/java/com/testing/Class2.java
@@ -0,0 +1,11 @@
+package com.testing;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("i am in my local workspace");
+
+	}
+
+}
